29f721445b70202c812dc1a455578a59355282c2
Pete Richardson <pete@peterichardson.com>
2023-01-16 11:45:55 -0800

    break out movement and moving a little more clearly.     Improved Ant.init() and default values
    specifically, separate calculation of move and turn from actual moving and turning

--- ants/Ant.swift
+++ ants/Ant.swift
@@ -5,47 +5,79 @@
 //  Created by Peter Richardson on 1/13/23.
 //
 
-import Cocoa
-import SwiftUI
 import SpriteKit
 
-class Ant: SKSpriteNode {
+class Ant : SKSpriteNode {
+    
+    var textureFileName : String = "blackant"
+    static let defaultMass : CGFloat = 1.0
+    static let maxAngleDelta : CGFloat = .pi / 18.0  // randomly change direction by at most ±maxDeltaAngle... ≈ 10˚
+    static let defaultColor = SKColor.black
+    static let defaultSize = CGSize(width: 15, height: 15)
+    static let defaultPosition = CGPoint(x: screenSize.width/2, y: screenSize.height/2)
+    static let defaultDirection = 0.0
 
-    init(color: SKColor, size: CGSize) {
-        let antfilename : String = (color == .red) ? "redant" : "blackant"
-        let texture = SKTexture.init(imageNamed: antfilename)
-        super.init(texture: texture, color: color, size: size)
+    var direction : CGFloat = Ant.defaultDirection
+    let moveSpeed = 1.0
+
+    init(texture: SKTexture? = nil,
+         color: SKColor? =  Ant.defaultColor,
+         size: CGSize? = Ant.defaultSize,
+         position: CGPoint? = nil,
+         direction: CGFloat? = nil) {
+ 
+        super.init(texture: texture ?? Ant.loadTextureForColor(.black),
+                   color: color ?? .black,
+                   size: size ?? CGSize(width: 15, height: 15))
+        self.position = position ?? CGPoint(x: CGFloat.random(in: 0...screenSize.width), y: CGFloat.random(in: 0...screenSize.height))
+        self.direction = direction ?? CGFloat.random(in: -.pi ... .pi)
+ 
         self.physicsBody = SKPhysicsBody(rectangleOf: self.size)
         self.physicsBody?.friction = 100.0
         self.physicsBody!.linearDamping = CGFloat(10)
         self.physicsBody!.angularDamping = CGFloat(5.0)
         self.physicsBody!.isDynamic = true
-        self.physicsBody!.mass = 1
-        self.direction = CGFloat.random(in: -.pi ... .pi)
+        self.physicsBody!.mass = Ant.defaultMass
+
+        self.run(SKAction.rotate(toAngle: self.direction, duration: 0.0))
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        self.run(SKAction.rotate(toAngle: self.direction, duration: 0.0))
+        fatalError("SKSpriteNode.init?() was called but it's not implemented!")
     }
     
-    var direction : CGFloat = 0.0
+    private static func loadTextureForColor(_ color: SKColor) -> SKTexture {
+        let textureFileName = (color == .red) ? "redant" : "blackant"
+        return SKTexture.init(imageNamed: textureFileName)
+    }
     
-    private func turn(delta: CGFloat) {
-        self.direction = self.direction + delta
+    func nextDirection() -> CGFloat {
+        let maxAngleDelta : CGFloat = .pi / 18.0  // randomly change direction by at most ±maxDeltaAngle... ≈ 10˚
+        let newDirection = (self.direction + CGFloat.random(in: -maxAngleDelta ... maxAngleDelta)).truncatingRemainder(dividingBy: 2 * .pi)
+        assert(!newDirection.isNaN)
+        return newDirection
+    }
+    
+    func nextPosition() -> CGPoint {
+        let newX = self.position.x + self.moveSpeed * Darwin.cos(self.direction)
+        let newY = self.position.y + self.moveSpeed * Darwin.sin(self.direction)
+        assert(!newX.isNaN && !newY.isNaN)
+        return CGPoint(x: newX, y: newY)
+    }
+    
+    private func turn() {
+        self.direction = self.nextDirection()
         self.run(SKAction.rotate(toAngle: (.pi * 1.5) + self.direction, duration: 0.0))
     }
     
     func move() {
-        let newLength = 1.0
-        let dx = newLength * Darwin.cos(self.direction)
-        let dy = newLength * Darwin.sin(self.direction)
-        self.position = CGPoint(x: self.position.x + dx, y: self.position.y + dy)
+        self.position = self.nextPosition()
     }
+
     
     func update() {
-        let maxAngleDelta : CGFloat = .pi / 18.0  // randomly change direction by at most ±maxDeltaAngle... ≈ 10˚
-        turn(delta: CGFloat.random(in: -maxAngleDelta ... maxAngleDelta))
+        turn()
         move()
     }
 }
--- ants/GameScene.swift
+++ ants/GameScene.swift
@@ -14,15 +14,15 @@ class GameScene: SKScene {
 
     override func sceneDidLoad() {
         self.backgroundColor = .white
-
         physicsWorld.gravity = CGVector (dx: 0.0, dy: 0.0)
         
-        let numAnts = 200
+        let numAnts = 100
         for i in 1...numAnts {
             let s = Int.random(in: 13...21)
             let color : SKColor = (i % 2) != 0 ? .red : .black
-            let ant = Ant(color: color, size: CGSize(width: s, height: s))
-            ant.position = CGPoint(x: CGFloat.random(in: 0...screenSize.width), y: CGFloat.random(in: 0...screenSize.height))
+            let position = CGPoint(x: CGFloat.random(in: 0...screenSize.width), y: CGFloat.random(in: 0...screenSize.height))
+            let ant = Ant(color: color, size: CGSize(width: s, height: s), position: position)
+            
             addChild(ant)
             ants.insert(ant, at: 0)
         }
